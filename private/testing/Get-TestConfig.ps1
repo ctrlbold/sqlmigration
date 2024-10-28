582b8d34da1351e6b4a1bdba22e6a65cacbcdd0a
Chrissy LeMaire <clemaire@gmail.com>
2024-10-28 17:38:32 +0100

    whoops seems they added an extra one in core

--- private/testing/Get-TestConfig.ps1
+++ private/testing/Get-TestConfig.ps1
@@ -69,7 +69,12 @@ function Get-TestConfig {
     }
 
     # sometimes the enumeration doesn't work, so we'll just hardcode it
-    $config['CommonParameters'] = "Verbose", "Debug", "ErrorAction", "WarningAction", "InformationAction", "ProgressAction", "ErrorVariable", "WarningVariable", "OutVariable", "OutBuffer", "PipelineVariable", "InformationVariable"
+     if ($PSVersionTable.PSEdition -eq 'Core') {
+        $config['CommonParameters'] = "Verbose", "Debug", "ErrorAction", "WarningAction", "InformationAction", "ProgressAction", "ErrorVariable", "WarningVariable", "OutVariable", "OutBuffer", "PipelineVariable", "InformationVariable"
+     } else {
+        $config['CommonParameters'] = "Verbose", "Debug", "ErrorAction", "WarningAction", "InformationAction", "ErrorVariable", "WarningVariable", "OutVariable", "OutBuffer", "PipelineVariable", "InformationVariable"
+     }
+
 
     [pscustomobject]$config
 }
